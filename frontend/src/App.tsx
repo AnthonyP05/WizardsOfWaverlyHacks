d6fd9f13489dbf977c29d863bb0173697837cdb0
DParra657 <Diegoparra657@gmail.com>
2026-02-07 15:45:33 -0500

    Start

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-screen bg-gradient-to-br from-purple-900 to-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        WizardsOfWaverlyHacks
+      </h1>
+    </div>
+  );
+};
+
+export default App;
